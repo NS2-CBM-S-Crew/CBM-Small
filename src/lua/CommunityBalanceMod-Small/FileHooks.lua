b1d86481b6e0d514932d644f986dca9faf31d661
Fredrik81 <devnull81@gmail.com>
2025-04-12 11:24:45 +0200

    First modify push and cleanup

---
+++ src/lua/CommunityBalanceMod-Small/FileHooks.lua
@@ -0,0 +1,21 @@
+local modVersion = "1.0.0"
+local modName = "Community Balance Mod - Small"
+
+if g_communityBalanceModConfig then
+    Print("ERROR: You are not allowed to use this mod with the Community Balance Mod!")
+    Print("Please remove the Community Balance Mod is you want to use the small version.")
+    return
+end
+
+-- This is a small version of the Community Balance Mod, which is a mod that aims to balance the game by adjusting various parameters and settings.
+Print("Loading CBS as base mod...")
+Script.load("lua/CommunityBalanceMod/FileHooks.lua")
+
+--Clear the CBS global variable
+g_communityBalanceModConfig = nil
+
+-- Load the extra settings for small mod
+ModLoader.SetupFileHook("lua/Balance.lua", "lua/CommunityBalanceMod/Balance.lua", "post")
+ModLoader.SetupFileHook("lua/Globals.lua", "lua/CommunityBalanceMod/Globals.lua", "post")
+--ModLoader.SetupFileHook("lua/Globals.lua", "lua/CommunityBalanceMod/Globals.lua", "replace")
+
