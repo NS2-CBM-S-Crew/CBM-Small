d79af07c1ff9f1066316ea3289f4ac89e1fc48f7
godsfoolcat <7934118+godsfoolcat@users.noreply.github.com>
2025-08-24 02:38:56 +0600

    50 more armor for onos

--- src/lua/CommunityBalanceMod-Small/BalanceHealth.lua
+++ src/lua/CommunityBalanceMod-Small/BalanceHealth.lua
@@ -7,6 +7,9 @@ kMatureCystHealth = 300
 -- Lerk
 kLerkHealth = 170
 
+-- Onos
+kOnosHealth = 700    kOnosArmor = 500    kOnosPointValue = 30    kOnosHealtPerBioMass = 50
+
 -- ARC
 kARCHealth = 2000
 kARCDeployedHealth = 2000
